ab7ff4ab65ba281c94fd12f86614bfaaa538ba46
chobham494 <chobham494@gmail.com>
2019-07-28 12:03:59 +0900

    cookieの実装修正

---
+++ js/common.js
@@ -0,0 +1,33 @@
+window.onload = function() {
+    getCookieData();
+}
+
+function login(){
+    var usernameVal= $("#Username").val();
+    var passwordVal= $("#Password").val();
+    document.cookie ="Username=usernameVal;Password=passwordVal;max-age=15552000";
+    getCookieData();
+}
+
+function logout(){
+    document.cookie = "Username=; Password=; max-age=0";
+    getCookieData();
+}
+
+function getCookieData(){
+    //cookie値を連想配列として取得する
+    var arr = new Array();
+    if(document.cookie != ''){
+      var tmp = document.cookie.split('; ');
+      for(var i=0;i<tmp.length;i++){
+        var data = tmp[i].split('=');
+        arr[data[0]] = decodeURIComponent(data[1]);
+      }
+    var username = arr["Username"];
+    var password = arr["Password"];
+    $('#UsernameCokkie').val(username);
+    $('#PasswordCokkie').val(password);
+    }
+};
+
+
